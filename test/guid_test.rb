29117926605d97e0022186af926b0ce4af74f4ad
Stephen Vance <svance@zendesk.com>
2020-01-17 10:03:10 -0500

    Make the test resistant to exexecution order

--- test/guid_test.rb
+++ test/guid_test.rb
@@ -1,18 +1,26 @@
-require File.join(File.dirname(__FILE__), 'test_helper')
+require_relative './test_helper'
 
 module Resque::Durable
   class GUIDTest < MiniTest::Unit::TestCase
 
     describe 'GUID generate' do
 
-      it 'has the hostname, process id and current time' do
-        hostname     = `hostname`.chomp
-        current_time = Time.now
-        process_id   = Process.pid
+      before do
+        @hostname     = `hostname`.chomp
+        @current_time = Time.now
+        @process_id   = Process.pid
+      end
 
-        Timecop.freeze(current_time) do
-          assert_equal "#{hostname}/#{process_id}/#{current_time.to_i}/1", GUID.generate
-          assert_equal "#{hostname}/#{process_id}/#{current_time.to_i}/2", GUID.generate
+      it 'has the hostname, process id and current time' do
+        assert_match /#{@hostname}\/#{@process_id}\/#{@current_time.to_i}\/\d+/, GUID.generate
+      end
+
+      it 'increments the generation counter' do
+
+        Timecop.freeze(@current_time) do
+          first = GUID.generate
+          counter = first.split(/\//)[-1].to_i + 1
+          assert_equal "#{@hostname}/#{@process_id}/#{@current_time.to_i}/#{counter}", GUID.generate
         end
 
       end
