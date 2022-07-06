8d92a64410853a75e81ff2689b3b557bf6dde284
Vatsal Sevak <vsevak@zendesk.com>
2022-07-06 14:34:40 -0700

    Generate random string for GUID

--- gemfiles/common.rb
+++ gemfiles/common.rb
@@ -1,13 +1,14 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 # Trigger build
 
-gemspec :path => '..'
+gemspec path: '..'
 
 gem 'bump'
 gem 'rake'
-gem 'resque',        '~>1.27'
+gem 'resque', '~>1.27'
 gem 'minitest'
 gem 'minitest-rg'
 gem 'mocha', '>=1.4'
 gem 'timecop'
+gem 'uuidtools', '~> 2.1.5'
 gem 'pry'
